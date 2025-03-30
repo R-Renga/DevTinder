069a51063bb112ea38dabf963ddef68f015872c8
R-Renga <rengaraja.ramamoorthy@chainsys.com>
2025-03-30 17:32:06 +0530

    formatted

--- src/app.js
+++ src/app.js
@@ -1,22 +1,17 @@
-const express = require('express');
-const {auth} = require('./middleware/auth')
+const express = require("express");
+const { auth } = require("./middleware/auth");
 const app = express();
 
-
-
-app.use("/admin",auth)
+app.use("/admin", auth);
 
 app.get("/admin/alldata", (req, res) => {
-
-    res.send("all data send")
+  res.send("all data send");
 });
 
 app.delete("/admin/deletingData", (req, res) => {
-    res.send("data deleted")
-})
-
-
+  res.send("data deleted");
+});
 
 app.listen(7777, () => {
-    console.log("server running port 7777")
-})
+  console.log("server running port 7777");
+});

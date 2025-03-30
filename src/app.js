f93e65d9c08c85678ca5deffec82f92183026c33
R-Renga <rengaraja.ramamoorthy@chainsys.com>
2025-03-30 17:58:34 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const { auth } = require("./middleware/auth");
+
 const app = express();
 
-app.use("/admin", auth);
 
-app.get("/admin/alldata", (req, res) => {
-  res.send("all data send");
-});
 
-app.delete("/admin/deletingData", (req, res) => {
-  res.send("data deleted");
-});
+app.get("/admin",(req,res)=>{
+    throw new Error("errorr")
+})
+
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("error occured")
+    }
+})
+
 
 app.listen(7777, () => {
   console.log("server running port 7777");
